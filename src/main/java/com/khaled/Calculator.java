e7e324243d0134a48b5dcd66ee96d826c8dd262d
KhaledWielki <adam.rezner95@gmail.com>
2017-06-27 23:35:17 +0200

    refactorization

--- src/main/java/com/khaled/Calculator.java
+++ src/main/java/com/khaled/Calculator.java
@@ -4,10 +4,18 @@ package com.khaled;
  * Created by Adam on 2017-06-27.
  */
 public class Calculator {
-    public int calculate(String string) {
-        if(string.isEmpty()){
+    public int calculate(String input) {
+        if(isEmpty(input)){
             return 0;
         }
-        return Integer.parseInt(string);
+        return parseStringToInt(input);
+    }
+
+    private boolean isEmpty(String input) {
+        return input.isEmpty();
+    }
+
+    private int parseStringToInt(String input) {
+        return Integer.parseInt(input);
     }
 }
