884dbbe83734cd6e310d0a252906a8138e1be78d
KhaledWielki <adam.rezner95@gmail.com>
2017-07-01 01:07:34 +0200

    single value is replied

--- src/main/java/com/khaled/Calculator.java
+++ src/main/java/com/khaled/Calculator.java
@@ -4,7 +4,10 @@ package com.khaled;
  * Created by Adam on 2017-06-27.
  */
 public class Calculator {
-    public int calculate(String string) {
-        return 0;
+    public int calculate(String input) {
+        if(input.isEmpty()) {
+            return 0;
+        }
+        return Integer.parseInt(input);
     }
 }
